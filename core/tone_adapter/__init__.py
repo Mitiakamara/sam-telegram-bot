b11f1801e329e2499e0efc382e979d2cea742bee
Mitiakamara <55333289+Mitiakamara@users.noreply.github.com>
2025-11-03 17:28:35 -0500

    Update __init__.py

--- core/tone_adapter/__init__.py
+++ core/tone_adapter/__init__.py
@@ -1,2 +1,7 @@
 # __init__.py
 # Indica que este directorio es un paquete Python
+# y facilita el acceso directo a ToneAdapter desde core.tone_adapter
+
+from .tone_adapter import ToneAdapter
+
+__all__ = ["ToneAdapter"]
